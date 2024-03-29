ef042bb8e31ece511038863de8228918023ce6e6
Avik-creator <avikm744@gmail.com>
2024-03-29 17:47:55 +0530

    Added Some Changes in The Routes.

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -11,8 +11,6 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
-
 router
     .route("/")
     .get(getAllVideos)
@@ -30,6 +28,8 @@ router
         ]),
         publishAVideo
     );
+
+    router.use(verifyJWT);
 
 router
     .route("/:videoId")
